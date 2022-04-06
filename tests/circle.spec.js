cd0b4ce1854f2e064874fb51e649415b5d3994e6
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-06 18:01:25 -0300

    feito requisito 04 - implementação do teste da função "circle"

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -24,15 +24,23 @@ const circle = require('../src/circle');
 */
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
-  it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    fail('Teste vazio!');
+  it('Verifica se ao receber um raio, a função `circle` retorna um objeto contendo os valores esperados', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
+    expect(circle('a')).toBeUndefined();
     // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado tem 3 propriedades.
+    expect(typeof circle(5)).toBe('object');
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se dentro do objeto retornado, a função retorna uma key com value igual a circunferência correta para um círculo de raio 2.
-    // Teste se dentro do objeto retornado, a função retorna uma key com value igual a área correta para um círculo de raio 3.
+    expect(circle()).toBeUndefined();
+    // Teste se o objeto retornado tem 3 propriedades.
+    expect(Object.keys(circle(5))).toHaveLength(3);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    expect(circle(3)).toHaveProperty('area');
+    expect(circle(3)).toHaveProperty('circumference');
+    expect(circle(3)).toHaveProperty('radius');
+    // Teste se dentro do objeto retornado, a função retorna uma key com value igual a circunferência correta para um círculo de raio 2.
+    expect(circle(2).circumference).toBeCloseTo(12.56);
+    // Teste se dentro do objeto retornado, a função retorna uma key com value igual a área correta para um círculo de raio 3.
+    expect(circle(3).area).toBeCloseTo(28.26);
   });
 });
