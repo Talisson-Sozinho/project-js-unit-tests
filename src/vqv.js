7add951eecfdee814f163dac437f3aeb9928201a
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-06 16:17:57 -0300

    feito requisito 03 - implementação da função "vqv"

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  if (!name || !age) return undefined;
+
+  const text = `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+
+  return text;
+};
 
 module.exports = vqv;
