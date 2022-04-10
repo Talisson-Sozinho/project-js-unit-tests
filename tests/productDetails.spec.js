7cd8d8f92c5ec6c086409e356986e553f70a1e2f
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-04-10 01:16:48 -0300

    fix ".toBeFalsy and .toBeTruthy" for ".toBe(false) and .toBe(true)"

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -32,7 +32,7 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste se productDetails é uma função.
     expect(typeof productDetails).toBe('function');
     // Teste se o retorno da função é um array.
-    expect(Array.isArray(productDetails('Álcool gel', 'Máscara'))).toBeTruthy();
+    expect(Array.isArray(productDetails('Álcool gel', 'Máscara'))).toBe(true);
     // Teste se o array retornado pela função contém dois itens dentro.
     expect(productDetails('Álcool gel', 'Máscara')).toHaveLength(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
